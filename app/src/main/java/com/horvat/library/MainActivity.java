2b72d517d3cc99aee4405f9e1a8f4f94b6e404f3
JosipHorvat <josiph988@gmail.com>
2020-11-19 17:20:39 +0100

    Library First page with different buttons. Added custom font for text view;

--- app/src/main/java/com/horvat/library/MainActivity.java
+++ app/src/main/java/com/horvat/library/MainActivity.java
@@ -3,12 +3,28 @@ package com.horvat.library;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAllBooks, btnAlreadyRead, btnWantToRead, btnCurrentlyReading, btnFavorite, btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+
+
+    }
+
+    private void initViews() {
+            //BUTTONS
+        btnAllBooks = findViewById(R.id.btnAllBooks);
+        btnAlreadyRead = findViewById(R.id.btnAlreadyRead);
+        btnWantToRead = findViewById(R.id.btnWantToReadBooks);
+        btnCurrentlyReading = findViewById(R.id.btnCurrentlyReading);
+        btnFavorite = findViewById(R.id.btnFavorite);
+        btnAbout = findViewById(R.id.btnAbout);
     }
 }
