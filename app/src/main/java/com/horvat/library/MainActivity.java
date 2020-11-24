19745033882b119a39c8b1b1c47ff91099f4c6ef
JosipHorvat <josiph988@gmail.com>
2020-11-24 12:35:15 +0100

    WebView: button "ABOUT" shows user this gitHub repository.

--- app/src/main/java/com/horvat/library/MainActivity.java
+++ app/src/main/java/com/horvat/library/MainActivity.java
@@ -1,7 +1,9 @@
 package com.horvat.library;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -56,6 +58,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,FavoriteBooksActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(getString(R.string.app_name));
+                builder.setMessage("Ovo je moj prva android aplikacija\n"+
+                        "Za kod ove aplikacije posjeti moju gitHub stranicu:P");
+                builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.setPositiveButton("Visit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(MainActivity.this, WebSiteActivity.class);
+                        intent.putExtra("url", "https://github.com/JosipHorvat/Library/commits/master");
+                        startActivity(intent);
+                    }
+                });
+                builder.create().show();
             }
         });
 
